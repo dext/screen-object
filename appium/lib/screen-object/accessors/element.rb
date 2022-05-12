3774dcc4cb71c6bfca2bb342310822b4e4232c95
ivodimitrov <ivopdimitrov@gmail.com>
2022-05-12 15:12:12 +0300

    RuboCop: Extra empty line detected

--- appium/lib/screen-object/accessors/element.rb
+++ appium/lib/screen-object/accessors/element.rb
@@ -9,14 +9,12 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License. 
+# See the License for the specific language governing permissions and limitations under the License.
 # ***********************************************************************************************************
 
 module ScreenObject
   module AppElements
-
     class Element
-
       attr_reader :locator
 
       def initialize(locator)
@@ -138,7 +136,6 @@ module ScreenObject
           element.click
         end
       end
-
     end
   end
 end
