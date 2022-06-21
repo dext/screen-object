eeaca24562653888dd35f7f845e003edbd9adb4b
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-21 14:59:04 +0300

    Fix rubocop offences at accessor button & button spec

--- spec/screen-object/button_spec.rb
+++ spec/screen-object/button_spec.rb
@@ -1,23 +1,20 @@
-require_relative '../spec_helper'
-
 describe 'Interface' do
-
   let(:selenium_object) { double('') }
-  let(:button) {ScreenObject::AppElements::Button.new(name: 'dummy')}
+  let(:button) { ScreenObject::AppElements::Button.new(name: 'dummy') }
 
   describe 'interaction with tap method' do
     example 'should tap on the button' do
-      expect(button).to receive(:element).and_return(selenium_object)
-      expect(selenium_object).to receive(:click).and_return(true)
-      expect(button.tap).to eq(true)
+      allow(button).to receive(:element).and_return(selenium_object)
+      allow(selenium_object).to receive(:click).and_return(true)
+      expect(button.tap).to be(true)
     end
   end
 
   describe 'interaction with enable method' do
     example 'should check if button is enabled' do
-      expect(button).to receive(:element).and_return(selenium_object)
-      expect(selenium_object).to receive(:enabled?).and_return(true)
-      expect(button.enabled?).to eq(true)
+      allow(button).to receive(:element).and_return(selenium_object)
+      allow(selenium_object).to receive(:enabled?).and_return(true)
+      expect(button.enabled?).to be(true)
     end
   end
 end
