44fcc4dd6faf8b7b0ce28162f18e47f2affc377a
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-21 15:09:16 +0300

    Fix rubocop offences at appium server spec

--- spec/integration/appium_server_spec.rb
+++ spec/integration/appium_server_spec.rb
@@ -1,22 +1,21 @@
 require 'screen-object/appium_server'
 
 describe ScreenObject::AppiumServer do
-
   let(:the_server) { ScreenObject::AppiumServer.new }
 
   context 'when appium server is started' do
-    it 'should start the appium server as a process' do
+    example 'should start the appium server as a process' do
       the_server.start
       the_pid = the_server.process.pid
       expect(the_server.process).to be_running_process
-      Process.kill(9, the_pid)
+      Process.kill 9, the_pid
     end
 
-    it 'should be stopped when exiting' do
+    example 'should be stopped when exiting' do
       the_server.start
-      the_pid = the_server.process.pid
+
       the_server.stop
-      expect(the_server.process).to_not be_running_process
+      expect(the_server.process).not_to be_running_process
     end
   end
 end
