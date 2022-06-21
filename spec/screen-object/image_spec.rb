b3f0995aa0726443bf920c96fb47b151450514a0
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-21 14:11:34 +0300

    Fix rubocop offences at accessor image & image spec

--- spec/screen-object/image_spec.rb
+++ spec/screen-object/image_spec.rb
@@ -1,18 +1,12 @@
-require_relative '../spec_helper'
-
 describe 'Interface' do
+  let(:selenium_driver) { double('') }
+  let(:image) { ScreenObject::AppElements::Image.new(name: 'dummy') }
 
-  let(:selenium_driver) {double('')}
-  let(:image) {ScreenObject::AppElements::Image.new(name: 'dummy')}
-
-  context 'interaction with click method' do
-    it 'should click on element' do
-      expect(image).to receive(:element).and_return(selenium_driver)
-      expect(selenium_driver).to receive(:click).and_return(true)
+  context 'when interaction with click method' do
+    it 'clicks on element' do
+      allow(image).to receive(:element).and_return(selenium_driver)
+      allow(selenium_driver).to receive(:click).and_return(true)
       expect(image.click).to be true
     end
   end
 end
-
-
-
