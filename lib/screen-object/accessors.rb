e302fcce3f0be42a7b3639b8b16452bd043aa48d
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-22 17:03:09 +0300

    Add checking the existence of the table

--- lib/screen-object/accessors.rb
+++ lib/screen-object/accessors.rb
@@ -1,17 +1,3 @@
-# ***********************************************************************************************************
-# SPDX-Copyright: Copyright (c) Capital One Services, LLC
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2016 Capital One Services, LLC
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
-# ***********************************************************************************************************
-
 module ScreenObject
   # contains  module level methods that are added into your screen objects.
   # when you include the ScreenObject module.  These methods will be generated as services for screens.
@@ -488,9 +474,21 @@ module ScreenObject
 
     # table class generates all the methods related to different operations that can be performed on the table object on the screen.
     def table(name, locator)
-      # generates method for counting total no of cells in table
-      define_method "#{name}_cell_count" do
-        ScreenObject::AppElements::Table.new(locator).cell_count
+      # generates method for checking the existence of the table.
+      # this method will return true or false based on object displayed or not.
+      # @example check if 'Item' table exists on the screen.
+      # table :item_table, xpath: '//XCUIElementTypeTable'
+      # DSL to check existence of item table
+      # def check_item_table
+      #  item_table? # This will return true or false based on existence of table.
+      # end
+      define_method "#{name}?" do
+        ScreenObject::AppElements::Table.new(locator).exists?
+      end
+
+      # generates method for counting total number of rows in table
+      define_method "#{name}_count_rows" do
+        ScreenObject::AppElements::Table.new(locator).count_rows
       end
 
       # returns the underlying ScreenObject element to allow
