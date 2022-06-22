e8f337a466a857abd5a4a6452dac30a6510458fb
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-22 17:02:37 +0300

    Rename specs describes

--- spec/screen-object/text_spec.rb
+++ spec/screen-object/text_spec.rb
@@ -1,6 +1,6 @@
-describe 'Interface' do
+describe 'Text' do
   let(:selenium_driver) { double('') }
-  let(:text) { ScreenObject::AppElements::TextField.new(name: 'dummy') }
+  let(:text) { ScreenObject::AppElements::Text.new(name: 'dummy') }
 
   context 'when interaction with clear method' do
     example 'clears the text field' do
@@ -11,7 +11,7 @@ describe 'Interface' do
 
     example 'clears the text field with dynamic text' do
       allow(text).to receive(:dynamic_xpath).with('dynamic_text').and_return(selenium_driver)
-      expect(selenium_driver).to receive(:displayed?).and_return(selenium_driver).and_return(true)
+      allow(selenium_driver).to receive(:displayed?).and_return(selenium_driver).and_return(true)
       expect(text.dynamic_text_exists?('dynamic_text')).to be true
     end
   end
