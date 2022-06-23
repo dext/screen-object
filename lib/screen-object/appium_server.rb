e51ecdd2fc039ca859b778a9d9447c7db84ebc1b
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-23 16:23:28 +0300

    Remove license comments

--- lib/screen-object/appium_server.rb
+++ lib/screen-object/appium_server.rb
@@ -1,24 +1,8 @@
-# ***********************************************************************************************************
-# SPDX-Copyright: Copyright (c) Capital One Services, LLC
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2016 Capital One Services, LLC
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
-# ***********************************************************************************************************
-
 require 'childprocess'
 
-#
-# This class is designed to start and stop the appium server process.  In order to use
-# it you must have appium and node installed on your computer.  You can pass parameters
+# This class is designed to start and stop the appium server process. In order to use
+# it you must have appium and node installed on your computer. You can pass parameters
 # to appium at startup via the constructor.
-#
 module ScreenObject
   class AppiumServer
     attr_accessor :process
