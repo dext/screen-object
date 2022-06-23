b1a59ca674d8437e69724654ec9f3c7248d6ba52
ivodimitrov <ivopdimitrov@gmail.com>
2022-06-23 15:09:53 +0300

    Remove empty blocks

--- spec/lib/screen_object_spec.rb
+++ spec/lib/screen_object_spec.rb
@@ -16,17 +16,17 @@ describe ScreenObject do
 
     example 'should wait for 5 seconds by default' do
       allow(Selenium::WebDriver::Wait).to receive(:new).with(timeout: 5, message: nil).and_return(wait)
-      the_screen.wait_until {}
+      the_screen.wait_until
     end
 
     example 'should allow you to override the default wait time' do
       allow(Selenium::WebDriver::Wait).to receive(:new).with(timeout: 10, message: nil).and_return(wait)
-      the_screen.wait_until(10) {}
+      the_screen.wait_until 10
     end
 
     example 'should allow you to provide a message to use when wait fails' do
       allow(Selenium::WebDriver::Wait).to receive(:new).with(timeout: 10, message: 'foobar').and_return(wait)
-      the_screen.wait_until(10, 'foobar') {}
+      the_screen.wait_until 10, 'foobar'
     end
   end
 end
